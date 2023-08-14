d7310e16490094c2e6a9acfe3e9ef4ffc3dba3e6
MiralDalayka <89144753+MiralDalayka@users.noreply.github.com>
2023-08-14 13:55:05 +0300

    Delete test.dart

--- test.dart
+++
@@ -1,8 +0,0 @@
-MultiProvider(
-  providers: [
-    Provider<Something>(create: (_) => Something()),
-    Provider<SomethingElse>(create: (_) => SomethingElse()),
-    Provider<AnotherThing>(create: (_) => AnotherThing()),
-  ],
-  child: someWidget,
-)
