fffca72b468e0cf9c0a015fd8ce7dcd624442217
MiralDalayka <89144753+MiralDalayka@users.noreply.github.com>
2023-08-14 17:51:07 +0300

    Add files via upload

---
+++ Share/ahmad/main (1).cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+int main() {
+  /*
+  cout<<"I'm Se student \n";
+  cout<<"JUST"<<endl;
+  cout<<"****";
+
+  // */
+
+  ///////////
+  // cout<<"****************\n";
+  // cout<<"1234\t5"<<endl;
+  //cout<<"1234567\t8";
+
+  int x =7,y=3;
+  cout<<"X/Y = "<<x*1.0/y<<endl;//2
+  cout<<"X%Y = "<<x%y<<endl;//1
+
+  bool a = true,b=false;
+  cout<<(a and b)<<endl;
+  cout<<(a && b)<<endl;
+  
+  
+}
