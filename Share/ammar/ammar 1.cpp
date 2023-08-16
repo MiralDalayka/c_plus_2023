333e735be4e22609531d37757c4eb18668823004
ammaromari96 <141843579+ammaromari96@users.noreply.github.com>
2023-08-16 17:57:20 +0300

    Create ammar 1.cpp

---
+++ Share/ammar/ammar 1.cpp
@@ -0,0 +1,93 @@
+
+
+#include <iostream>
+using namespace std;
+int main() {
+  /*
+  int a,b;
+  cout<<"please enter two variable ";
+  cin>>a>>b;
+  a=a+b;
+  b=a-b;
+  a=a-b;
+    cout<<a<<b;
+
+  int age;
+  cout << "please enter your age";
+  cin >> age;
+  if (age < 0 && age > 100) {
+
+    if (age >= 18) {
+      cout << "you can vote";
+    } else
+      cout << "you cant vote";
+
+  } else {
+    cout << "worng input";
+
+
+int a,b,c;
+  cout<<"please enter 3 number ";
+  cin>>a>>b>>c;
+  if (a+b+c==180)
+    cout<<"this is a triangle";
+  else
+    cout<<"this is not triangle";
+
+
+  int a, b, c;
+  cout << "please enter 3 number ";
+  cin >> a >> b >> c;
+  if (a > b && a > c)
+    cout << "a is the largest =" << a;
+  else if ( b > c)
+    cout << "b is the largest =" << b;
+  else
+    cout << "c is the largerst =" << c;
+
+
+// next time b3aref 8emeh 3shan ma a9'l a3ml cout
+  float s;
+  cout << "please enter salary ";
+  cin >> s;
+  if (2000 < s)
+    cout << "bounce ratio =" << s * 0.15+s;
+  else if (1300 < s && s< 2000)
+    cout << "bounce ratio =" << s * 0.20+s;
+  else if (700 < s && s< 1300)
+    cout << "bounce ratio =" << s * 0.35+s;
+  else
+    cout << "bounce ratio =" << s * 0.40+s;
+ */
+int num;
+  cout<<"please enter number";
+  cin>>num;
+  if (num>0 && num%2==0)
+    cout<<"num is positive even";
+      else if (num<0 && num%-2==0)
+         cout<<"num is negative even";
+  else if(num>0 && num%2!=0)
+     cout<<"num is positive odd";
+  else 
+    cout<<"num is negative odd";
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
+}
