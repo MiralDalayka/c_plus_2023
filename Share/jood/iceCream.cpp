1c84021a1dcaa699aad8e73abbdbb1c254545108
Joud Al-jarrah <72281607+joudjarra75@users.noreply.github.com>
2023-08-16 22:50:57 +0300

    Update iceCream.cpp

--- Share/jood/iceCream.cpp
+++ Share/jood/iceCream.cpp
@@ -1,1 +1,29 @@
+#include <iostream>
+using namespace std;
+int budget;
+int price_of_icecream;
+int total;
+int rem;
+int number_of_icecream = 10;
+int main() {
+  cout << "enter price = " << endl;
+  cin >> price_of_icecream;
+  cout << "enter your budget = " << endl;
+  cin >> budget;
+  total=price_of_icecream*number_of_icecream;
 
+  if(total>budget)
+  {
+    cout << "Oops! you can't buy icecream for your friends :("<<endl;
+  }
+
+  else{
+    cout << "you can buy icecream for your friends :)" << endl;
+    rem = budget - (price_of_icecream * number_of_icecream);
+    cout << "your budget now = " << rem << endl;
+  }
+  
+  
+}
+
+  
