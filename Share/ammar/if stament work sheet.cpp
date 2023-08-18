48f95a0a7d1f6e20aa5df0d69441d8109fb75dde
ammaromari96 <141843579+ammaromari96@users.noreply.github.com>
2023-08-18 16:53:08 +0300

    Create if stament work sheet.cpp

---
+++ Share/ammar/if stament work sheet.cpp
@@ -0,0 +1,158 @@
+#include <iostream>
+using namespace std;
+int main() {
+/*
+//Write C++ code to convert temperature from Celsius to 
+//Fahrenheit
+float fahrenheit,Celsius;
+  cout<<"please enter temperature in celsius";
+  cin>>Celsius;
+  fahrenheit= Celsius * 1.8 + 32;
+    cout<<"the temprature in fahrenhit ="<<fahrenheit;
+
+
+//Write C++ code to Swap Two Numbers without using temporary 
+//variable
+
+  int num1,num2,num3;
+  cout<<"please enter 2 number to swap from each other";
+  cin>>num1>>num2;
+  num1=num1+num2;
+  num2=num1-num2;
+  num1=num1-num2;
+  cout<<num1<<num2;
+
+//Take values of length and breadth of a rectangle from user and 
+//check if it is square or not
+
+int length,breadth;
+  cout<<"please enter length then breadth";
+  cin>>length>>breadth;
+  if (length==breadth)
+cout<<"this is a rectangle ";
+     else 
+    cout<<"this is not rectangle";
+
+//Take two integers values from user and print greatest among 
+//them.
+
+  int x,y;
+  cout<<"please enter two value ";
+  cin>>x>>y;
+  if (x>y)
+    cout<<"X is the largest ="<<x;
+  else 
+    cout<<"Y is the largest ="<<y;
+
+
+  //Write C++ code to find the number of years, weeks and days in 
+//the input days number
+
+  int totalDays;
+  cout<<"please enter number of day";
+    cin>>totalDays;
+   int years = totalDays / 365;
+    int remainingDays = totalDays % 365;
+    int weeks = remainingDays / 7;
+    int days = remainingDays % 7;
+  cout<<"years : "<<years<<endl;
+   cout<<"weeks : "<<weeks<<endl;
+   cout<<"days : "<<days<<endl;
+  
+  
+
+ // Write C++ to find if the input year is a leap year or not
+
+  int daynumber;
+  cout<<"enter daynumber ";
+  cin>>daynumber;
+  if (daynumber==366)
+    cout<<"this is a leap year";
+  else
+    cout<<"this is a not leap year";
+  
+   
+
+  //Write C++ code to ask the user to enter any letter then 
+  //check if it’s vowel letter or not.
+
+  char letter;
+  cout<<"please enter any latter to check its a vowel letter  ";
+  cin>>letter;
+  if (letter=='a'||letter=='e'||letter=='i'||letter=='o'||letter=='u'||
+    letter=='A'||letter=='E'||letter=='I'||letter=='O'||letter=='U' 
+    )
+    cout<<"this is a vowel letter";
+  else 
+    cout<<"this is not a vowel latter";
+
+
+  //Write C++ program to ask the user to input the 
+//temperature if the temperature is less than 25 then print 
+//“cold” else print “Hot”.
+
+  int temp;
+  cout<<"please enter the temperature ";
+    cin>>temp;
+  if (temp<25)
+    cout<<"cold";
+      else 
+    cout<<"hot;
+
+
+
+ // Write C++ program to ask the user to enter the three 
+//angles then check if it’s formed a triangle or not.
+
+
+int angle1,angle2,angle3;
+  cout<<"please enter 3 angles to check if triangle or not";
+  cin>>angle1>>angle2>>angle3;
+  if (angle1+angle2+angle3==180)
+    cout<<"this is a triangle";
+  else 
+    cout<<"this is not triangle";
+
+
+
+
+  //Write a C++ program to check if the last digit of the 
+  //number is odd or even.
+
+   int number;
+
+    cout << "Enter a number: ";
+    cin >> number;
+
+    int lastDigit = number % 10;
+
+    if (lastDigit % 2 == 0) 
+        cout << "The last digit is even." << endl;
+     else 
+        cout << "The last digit is odd." << endl;
+    
+
+   */
+
+  
+
+
+
+
+
+
+
+  
+  
+
+  }
+    
+  
+
+
+
+
+
+
+  
+  
