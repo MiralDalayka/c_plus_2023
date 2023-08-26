e47a9c5b4ceee758f2c2c92d414cda88fa74a1df
Joud Al-jarrah <72281607+joudjarra75@users.noreply.github.com>
2023-08-26 15:54:18 +0300

    Update GCD.cpp

--- Share/jood/GCD.cpp
+++ Share/jood/GCD.cpp
@@ -1,24 +1,27 @@
-//Write C++ program to find the Greatest common divisor between two 
-//input numbers.
+// Write C++ program to find the Greatest common divisor between two
+// input numbers.
 #include <iostream>
 using namespace std;
 int main() {
   int n1, n2;
+  bool flag=false;
   cout << "enter two numbers:" << endl;
   cin >> n1 >> n2;
-  int max=n1;
-  int min=n2;
-  if(n2>n1)
-  {
-    max=n2;
-    min=n1;
+  int max = n1;
+  int min = n2;
+  if (n2 > n1) {
+    max = n2;
+    min = n1;
   }
-  for(int i=max;i>0;i--)
-    {
-      if(max%i==0 && min%i==0)
-      {
-        cout<<"GCD="<<i<<endl;
-        break;
-      }
+  for (int i = max; i > 0; i--) {
+    if (max % i == 0 && min % i == 0) {
+      cout << "GCD=" << i << endl;
+      flag=true;
+      break;
     }
+  }
+  if(flag==false)
+  {
+    cout << "there is no GCD between " << min << " and " << max << endl;
+  }
 }
