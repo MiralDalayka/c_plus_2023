ce5828f15ffcb1067eed0d29f7feb5f90b6e1822
MiralDalayka <89144753+MiralDalayka@users.noreply.github.com>
2023-08-28 17:12:14 +0300

    Create loop3.cpp

---
+++ Code/loop3.cpp
@@ -0,0 +1,34 @@
+
+
+#include <iostream>
+using namespace std;
+int main() {
+  /*
+
+  *****
+  *****
+  *****
+  *****
+  *****
+
+*/
+  // int row, col;
+  // cin >> row >> col;
+  // for (int i = 1; i <= row; i++) {
+
+  //   for (int c = 1; c <= col; c++) {
+  //     cout << "*";
+  //   }
+  //   cout << endl;
+  // }
+
+  // cout << "*****" << endl;
+
+  for (int i = 1; i <= 100; i++)
+
+  {
+    if (i == 3)
+      continue;
+    cout << i << " ";
+  }
+}
