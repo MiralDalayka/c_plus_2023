e47ff0db18073ab5fc378cef2ccf5931916316a3
MiralDalayka <89144753+MiralDalayka@users.noreply.github.com>
2023-09-02 17:20:42 +0300

    Update array_worksheet1_sol.cpp

--- Code/array_worksheet1_sol.cpp
+++ Code/array_worksheet1_sol.cpp
@@ -95,6 +95,132 @@ int main() {
     } else
       cout << "reapeating in " << word << endl;
   }
+  case 6: {
+    string list1[] = {"M", "na", "i", "Ke"}, list2[] = {"y", "me", "s", "lly"},
+           list3[4];
+    // int sise = sizeof(list1)/sizeof(list1[0]);
+    // int size  = 4;
+    for (int i = 0; i < 4; i++) {
+      list3[i] = list1[i] + list2[i]; // list3[0] = list1[0]+list2[0] = My
+    }
+
+    for (int i = 0; i < 4; i++) {
+      cout << list3[i] << " ";
+    }
+    break;
+  }
+  case 7: {
+    int numbers[] = {1, 2, 3, 4, 5, 6, 7};
+    int size = sizeof(numbers) / sizeof(numbers[0]);
+    int newArray[size];
+    for (int i = 0; i < size; i++) {
+      newArray[i] = numbers[i] * numbers[i]; // newArray[1] = 2 * 2 = 4
+    }
+
+    for (int i = 0; i < size; i++) {
+      cout << newArray[i] << " ";
+    }
+    cout << endl;
+    //// original array
+    for (int i = 0; i < size; i++) {
+      numbers[i] = numbers[i] * numbers[i];
+    }
+    for (int i = 0; i < size; i++) {
+      cout << numbers[i] << " ";
+    }
+
+    break;
+  }
+  case 8: {
+    string list1[] = {"Hello ", "take "}, list2[] = {"Dear", "Sir"};
+    string newList[4];
+    int newListCounter = 0;
+    int size1 = sizeof(list1) / sizeof(list1[0]),
+        size2 = sizeof(list2) / sizeof(list2[0]);
+    for (int i = 0; i < size1; i++) {
+      for (int j = 0; j < size2; j++) {
+        newList[newListCounter++] = list1[i] + list2[j]; // i=1
+        // newlist[0] = hello + dear  , newlist[1] = hello + sir
+      }
+    }
+
+    for (int i = 0; i < 4; i++) {
+      cout << newList[i] << ",";
+    }
+
+    break;
+  }
+  case 9: {
+    string list1[] = {"Mike", "", "Emma", "Kelly", "", "Brad"};
+    // int listOneSize = sizeof(list1)/sizeof(list1[0]);
+    int listOneSize = 6, list2Size = 0;
+
+    for (int i = 0; i < listOneSize; i++) {
+      if (list1[i].length() != 0) {
+        list2Size++;
+      }
+    }
+    string newList[list2Size];
+    list2Size = 0;
+    for (int i = 0; i < listOneSize; i++) {
+      if (list1[i].length() != 0) {
+        newList[list2Size++] = list1[i];
+      }
+    }
+
+    for (int i = 0; i < list2Size; i++) {
+      cout << newList[i] << " ";
+    }
+  }
+  case 10: {
+    int list1[] = {5, 10, 15, 20, 25, 50, 20};
+    int size = sizeof(list1) / sizeof(list1[0]);
+    // first occurrence
+    //  for(int i=0;i<size;i++){
+    //    if(list1[i]==20)
+    //    {
+    //      list1[i] = 200;
+    //      break;
+    //    }
+    //  }
+    // lase occurrence
+    for (int i = size - 1; i >= 0; i--) {
+      if (list1[i] == 20) {
+        list1[i] = 200;
+        break;
+      }
+    }
+    for (int i = 0; i < size; i++) {
+      cout << list1[i] << " ";
+    }
+
+    break;
+  }
+  case 11: {
+    int array[] = {20, 30, 40};
+    int sum = 0, avg;
+    for (int i = 0; i < 3; i++) {
+      sum += array[i];
+    }
+    cout << "Sum is =" << sum << endl << "Avg = " << sum / 3 << endl;
+    break;
+  }
+
+  case 12: {
+    // How do you find all pairs of an integer array whose sum is equal to a
+    // given number? 4
+    // [2 , 3 ,1 ,2 ] -> 2,2 - 3,1
+    int array[] = {15, 5, 20, 0, 10, 10}, sum,
+        size = sizeof(array) / sizeof(array[0]);
+    cout << "Enter the sum:";
+    cin >> sum;
+    for (int i = 0; i < size; i++) {
+      for (int j = i + 1; j < size; j++) {
+        if (array[i] + array[j] == sum)
+          cout << array[i] << "," << array[j] << endl;
+      }
+    }
+  }
   }
   return 0;
 }
