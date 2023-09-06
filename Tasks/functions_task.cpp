3c632ae40646bfd57e84d8a5d69b6d5b9ccb282e
MiralDalayka <89144753+MiralDalayka@users.noreply.github.com>
2023-09-06 16:06:51 +0300

    Create functions_task.cpp

---
+++ Tasks/functions_task.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+using namespace std;
+int main() {
+  /*
+  1- Write a C++ code to check if the number is positive or negative
+  -----------------------------------
+  2- Write a C++ code to check number is Even or odd.
+  -----------------------------------
+  3- Write a C++ code to find whether a given year is a leap year or not.
+  -----------------------------------
+  4- Write a C++ code to read the age of a candidate and determine whether it is
+  eligible for casting his/her own vote. input : 21 Expected Output :
+  Congratulation! You are eligible for casting your vote.
+  input :15
+  Expected Output :
+  You are not allowed to vote.
+  -----------------------------------
+  5- Write a C++ code to o find the largest of three numbers.
+  Input: 12 25 52
+  Expected Output :
+  1st Number = 12, 2nd Number = 25, 3rd Number = 52
+  The 3rd Number is the greatest among three.
+  -----------------------------------
+  6- Write a C++ code to check whether a triangle can be formed by the given
+  value for the angles. Input: 40 55 65 Expected Output : The triangle is not
+  valid.
+  -----------------------------------
+  7-Company wants to give bouse to it’s employees based on their
+  salaries:
+  Calculate how much each employee will get after bounce
+  salary Bounce ratio.
+  2000<S --> 15%
+  1300<S<2000 -->  20%
+  700<S<1300  --> 35%
+  less than 700 -->  40%
+  -----------------------------------
+  8-Write a Dart program to create menu driven calculator that
+  performs basic arithmetic operations (add, subtract, multiply and
+  divide) using switch case and functions. The calculator should input
+  two numbers and an operator from user.
+  -----------------------------------
+  9-Write a Dart program to input an alphabet and check whether it is
+  vowel or consonant using switch case.
+  */
+}
