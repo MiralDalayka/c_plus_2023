c98c7c73f1897c8994a9bed9f0919985a275d32b
ammaromari96 <141843579+ammaromari96@users.noreply.github.com>
2023-09-20 17:58:50 +0300

    Create BookShop.cpp

---
+++ Share/ammar/BookShop.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+class Books{
+int BookId;
+float BookPrice;
+string BookCategory,BookAuthor,BookStatus;
+public:
+Books(int BookId,float BookPrice,string BookCategory,string BookAuthor,string BookStatus){
+ Books::BookId = BookId;
+   Books::BookPrice = BookPrice;
+    Books::BookCategory =BookCategory;
+    Books::BookAuthor=BookAuthor;
+  Books::BookSatatus=BookStatus;
+
+
+
+
+  
+}
+
+
+
+
+
+};
+
+
+
+
+
+
+
+int main() {
+  
+}
